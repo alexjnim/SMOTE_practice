cc649c3d5ec3197c4a77f0d06a9e2770a85d2cb5
Alex Nim <alexnimjli@gmail.com>
2019-07-17 15:44:39 -0700

    adding 01 file

---
+++ 01_Data_Analysis.py
@@ -0,0 +1,55 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.7
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+import numpy as np
+import os
+
+np.random.seed(42)
+
+# %matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Ignore useless warnings
+import warnings
+warnings.filterwarnings(action="ignore", message="^internal gelsd")
+
+# +
+import pandas as pd
+pd.set_option('display.max_rows', 30)
+
+df = pd.read_csv("credit_train.csv")
+# -
+
+df.head()
+
+# let's drop Loan ID and Customer ID as this won't be used for modelling
+
+df = df.drop('Loan ID', axis =1)
+df = df.drop('Customer ID', axis=1)
+
+df.head()
+
+df.isnull().any(axis=0)
+
+df.isnull().sum()
+
+df.tail()
+
+
