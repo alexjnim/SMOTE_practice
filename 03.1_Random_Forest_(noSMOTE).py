936e47c8e864db85d50f57ae6560c4046e862427
Alex Nim <alexnimjli@gmail.com>
2019-07-19 09:25:04 -0700

    running results again for random forest

--- 03.1_Random_Forest_(noSMOTE).py
+++ 03.1_Random_Forest_(noSMOTE).py
@@ -53,6 +53,8 @@ X_pretest.shape
 
 X_train.shape
 
+y_train.value_counts()
+
 # +
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
@@ -69,10 +71,10 @@ print('accuracy: {}'.format(score))
 
 # 72% percent accuracy is pretty good! but remember, if the model predicts that all of the entries pay off their loans, we would get a 78.77% accuracy.
 
-print('Pays off loans:', round(y_train.value_counts()[1]/len(y_train) * 100,2), '% of the dataset')
-print('Does not pay off loans:', round(y_train.value_counts()[0]/len(y_train) * 100,2), '% of the dataset')
+print('Pays off loans:', round(y_train.value_counts()[0]/len(y_train) * 100,2), '% of the dataset')
+print('Does not pay off loans:', round(y_train.value_counts()[1]/len(y_train) * 100,2), '% of the dataset')
 
-# # let's do this with cross validation
+# # let's do this with cross validation to check the actual accuracy as it can test this against the validation set
 
 # +
 from sklearn.model_selection import cross_val_score
@@ -95,14 +97,13 @@ forest_scores = cross_val_score(forest_clf, X_train, y_train,
 
 display_scores(forest_scores)
 
-# # damn, the RMSE error is pretty bad
+# # 50% accuracy! Not good.
 #
-# # let's try GridSearch to find optimal setting for random forest
+# # let's try GridSearch to find optimal setting for random forest, maybe that'll help
 
 # +
 from sklearn.model_selection import GridSearchCV
 
-display_scores(forest_rmse_scores)
 param_grid = [
     {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
     {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
@@ -111,7 +112,7 @@ param_grid = [
 forest_clf = RandomForestClassifier(random_state=42)
 
 grid_search = GridSearchCV(forest_clf, param_grid, cv=5,
-                           scoring='neg_mean_squared_error', return_train_score=True)
+                           scoring='accuracy', return_train_score=True)
 
 
 grid_search.fit(X_train, y_train)
@@ -123,11 +124,16 @@ grid_search.best_estimator_
 
 cvres = grid_search.cv_results_
 for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
-    print(np.sqrt(-mean_score), params)
+    print(mean_score, params)
 
 best_model = grid_search.best_estimator_
 
-rmse_value(X_pretest, y_pretest, best_model)
+feature_importances = grid_search.best_estimator_.feature_importances_ 
+feature_importances
+
+# # here we can decide which features are important or not, and therefore drop them! 
+
+X_train.columns
 
 # # let's use confusion matrix to evaluate the error properly 
 
@@ -154,7 +160,7 @@ print(confusion_mat, "\n")
 print(normalised_confusion_mat)
 # -
 
-# # basically predicted most Loan Status to be 1 
+# # basically predicted most Loan Status to be 0 (Fully Paid). This is terrible 
 
 from sklearn.metrics import accuracy_score
 print(accuracy_score(y_pretest_pred, y_pretest))
@@ -168,54 +174,6 @@ print('the recall score is : ', recall_score(y_pretest, y_pretest_pred))
 print('the f1 score is : ', f1_score(y_pretest, y_pretest_pred))
 # -
 
-y_train.value_counts()
-
-y_pretest.shape
-
-# # let's look at the precision and recall curve
-
-# +
-from sklearn.metrics import precision_recall_curve
-
-precisions, recalls, thresholds = precision_recall_curve(y_pretest_pred, y_pretest)
-
-def plot_precision_recall_vs_threshold(precisions, recalls, thresholds): 
-    plt.plot(thresholds, precisions[:-1], "b--", label="Precision") 
-    plt.plot(thresholds, recalls[:-1], "g-", label="Recall") 
-    plt.xlabel("Threshold")
-    plt.legend(loc="lower left") 
-    plt.ylim([0, 1])
-    
-plot_precision_recall_vs_threshold(precisions, recalls, thresholds) 
-plt.show()
-# -
-
-# # let's look at the learning curve
-
-# +
-from sklearn.model_selection import train_test_split
-
-def plot_learning_curves(X, y, model, title, starting_point):
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
-    train_errors, val_errors = [], []
-    for m in range(starting_point, len(X_train)):
-        model.fit(X_train[:m], y_train[:m])
-        y_train_predict = model.predict(X_train[:m])
-        y_val_predict = model.predict(X_val)
-        train_errors.append(mean_squared_error(y_train_predict, y_train[:m]))
-        val_errors.append(mean_squared_error(y_val_predict, y_val))
-    plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train") 
-    plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
-    plt.legend(loc='upper right')
-    plt.title(title)
-    plt.xlabel('Training set size')
-    plt.ylabel('RMSE')
-
-    plt.show()
-
-
-# -
-
-plot_learning_curves(X_train, y_train, best_model, "Best Random Forest predictor", 1)
+# # these scores are terrible! 
 
 
